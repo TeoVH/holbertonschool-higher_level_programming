976420aca58e66c0cedb539f357b1c44ea0c43ef
TeoVH <villadamateo64@gmail.com>
2022-05-03 23:28:59 -0500

    Code to print a random number and to know if is positive or negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ else:
 
 if module > 5:
     print(f"Last digit of {number} is {module} and is greater than 5")
-elif module is 0:
+elif module == 0:
     print(f"Last digit of {number} is {module} and is 0")
 else:
     print(f"Last digit of {number} is {module} and is less than 6 and not 0")
