b354efc46e83158de1f9bb8685169ab3bca97a18
TeoVH <villadamateo64@gmail.com>
2022-06-07 18:55:52 -0500

    Class BaseGeometry with public instance methods

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+7. Integer validator
+Class BaseGeometry with public instance methods
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+
+    Methods:
+        area()
+        integer_validator()
+    """
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Function that validates value
+        """
+        if type(name) is not str:
+            raise TypeError("name must be an integer")
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
