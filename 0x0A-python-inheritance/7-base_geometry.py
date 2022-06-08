338e5ecb3a1fd94cf9d154dedb876c1426b32a9d
TeoVH <villadamateo64@gmail.com>
2022-06-07 22:54:04 -0500

    Class BaseGeometry with public methods

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-
-'''Write a class BaseGeometry (based on 6-base_geometry.py)'''
+"""
+Class BaseGeometry with publoc methods
+"""
 
 
 class BaseGeometry:
-    '''Initialize the class BaseGeometry'''
+    """
+    class BaseGeometry
+    """
+
     def area(self):
-        raise Exception("area() is not implemented")
+        """
+        raises an Exception message
+        """
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(name + " must be an integer")
+        """
+        public method
+        """
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError('{} must be greater than 0'.format(name))
