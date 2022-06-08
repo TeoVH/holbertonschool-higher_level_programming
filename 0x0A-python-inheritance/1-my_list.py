290f52457198019ccba97d373282eb083e2e4f52
TeoVH <villadamateo64@gmail.com>
2022-06-07 21:41:36 -0500

    Class My_list with a public method that print the list sorted

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+1. My list
+Class My_list with a public method that print the list sorted
+"""
+
+
+class MyList(list):
+    """
+    Class MyList that inherits from list
+    """
+    def print_sorted(self):
+        """
+        Method that prints the list, but sorted
+        """
+        print(sorted(self))
