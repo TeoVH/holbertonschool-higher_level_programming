6642ad24de17034c0c9de9e204e9d6010d39892f
TeoVH <villadamateo64@gmail.com>
2022-06-08 23:39:28 -0500

    Class Student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student:
+    """
+    Class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Public method that retrieves a dictionary
+        representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for i in attrs:
+            if i in self.__dict__:
+                dict[i] = self.__dict__[i]
+        return dict
+
+    def reload_from_json(self, json):
+            """
+            Public method that replaces all attributes
+            of the Student instance
+            """
+            for i, x in json.items():
+                self.__dict__[i] = x
