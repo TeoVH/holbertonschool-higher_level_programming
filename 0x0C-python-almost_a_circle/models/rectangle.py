14b5826e22e327dbdcf10a4529f87c6dea4654b8
TeoVH <villadamateo64@gmail.com>
2022-06-14 14:51:02 -0500

    Comments Fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-class Rectangle: class Rectangle that inherits from Base
-"""
+""" class Rectangle: class Rectangle that inherits from Base """
 from models.base import Base
 
 
 class Rectangle(Base):
     """
-    Initialize the class
+    Class Rectangle that inherits from Base
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        class Rectangle that inherits Base
+        Constructor
         """
         super().__init__(id)
         self.width = width
