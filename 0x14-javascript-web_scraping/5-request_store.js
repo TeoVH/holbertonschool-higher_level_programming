2e0df79663072d9379fb7a059dd1e8163ce7ca8b
TeoVH <villadamateo64@gmail.com>
2022-08-17 22:05:54 -0500

    script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 const request = require('request');
 const fs = require('fs');
-request(url, function (err, body) {
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
