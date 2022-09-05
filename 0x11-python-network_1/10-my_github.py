3f2ff78ddc19abdd37653a541050bb6be6c9dea5
TeoVH <villadamateo64@gmail.com>
2022-09-04 23:21:16 -0500

    Script that takes your GitHub credentials and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that takes your GitHub credentials
+and uses the GitHub API to display your id
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    auth = (argv[1], argv[2])
+    response = requests.get(url, auth=auth)
+    try:
+        print(response.json().get("id"))
+    except ValueError:
+        print("Not a valid JSON")
